4565526560efdd54b958dcf762328024b8c35d54
Ilya Mochalov <2111311792@qq.com>
2017-01-04 16:23:35 +0800

    Added layout for index page

--- blog.py
+++ blog.py
@@ -1,13 +1,14 @@
 from flask_script import Manager
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return "index"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     manager.run()
